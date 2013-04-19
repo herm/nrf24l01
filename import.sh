7c5e866619e44c3480f234c4f5a86edc92fd65d0
Hermann Kraus <hermr2d2@gmail.com>
2013-04-19 14:08:28 +0200

    Remove import script.

--- import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#!/bin/sh
-sourcedir=$(dirname $(readlink -f $0))
-modulename=nrf24l01
-git submodule add "$sourcedir" "$modulename"
-echo "include $modulename/Makefile.module" >> Makefile.modules
